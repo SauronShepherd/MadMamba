437816f3381a16fe3ddf84f6f66091809a6dd552
__angel__ <angel.alvarez.pascua@gmail.com>
2026-09-03 07:30:04 +0200

    feat: export exclusive runtime ownership error

--- madmamba/__init__.py
+++ madmamba/__init__.py
@@ -1,12 +1,19 @@
 """MadMamba runtime package."""
 
 from .lifecycle import InterpreterRuntimeLifecycle
-from .runtime import CoverageGapError, InterpreterRuntimeRegistry, RuntimeKernel, current_interpreter_key
+from .runtime import (
+    CoverageGapError,
+    InterpreterRuntimeRegistry,
+    RuntimeAlreadyBootstrappedError,
+    RuntimeKernel,
+    current_interpreter_key,
+)
 
 __all__ = [
     "CoverageGapError",
     "InterpreterRuntimeLifecycle",
     "InterpreterRuntimeRegistry",
+    "RuntimeAlreadyBootstrappedError",
     "RuntimeKernel",
     "current_interpreter_key",
 ]
